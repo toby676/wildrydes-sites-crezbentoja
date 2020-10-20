53533b3449e03bed12776ee52f468d3b0d6309c1
Crescenzo D'Alterio <crescenzo.dalterio@simplybusiness.co.uk>
2020-10-20 14:28:00 +0100

    Update userpool id and clientid

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-2_jLsA0kApW', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1vskhtf81ue86rdfhlui9cqdbg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-west-2_8WDrGU0Qh', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6g0r6kg3bh4pku4gqsst41blah', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
